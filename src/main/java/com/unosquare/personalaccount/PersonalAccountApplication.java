02b504d8060b6a98e974629e311e760c64ee4258
Luis Kupferberg <lkupferberg@overactive.com>
2019-10-30 15:00:30 -0400

    -- Add all dependencies and necessary tools

---
+++ src/main/java/com/unosquare/personalaccount/PersonalAccountApplication.java
@@ -0,0 +1,12 @@
+package com.unosquare.personalaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalAccountApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PersonalAccountApplication.class, args);
+  }
+}
---
+++ src/test/java/com/unosquare/personalaccount/PersonalAccountApplicationTests.java
@@ -0,0 +1,11 @@
+package com.unosquare.personalaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalAccountApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
