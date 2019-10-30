b945bb602a00ddaac08f5ef1b0852bdc9d59dbeb
Luis Kupferberg <lkupferberg@overactive.com>
2019-10-30 15:34:15 -0400

    -- Adding test

---
+++ src/test/java/com/unosquare/personalaccount/TestCI.java
@@ -0,0 +1,13 @@
+package com.unosquare.personalaccount;
+
+import org.junit.Test;
+
+/**
+ * @author Luis M. Kupferberg Ruiz (lkupferberg@overactive.com)
+ * @created 2019-10-30 15:30
+ */
+public class TestCI {
+
+  @Test
+  public void test() {}
+}
