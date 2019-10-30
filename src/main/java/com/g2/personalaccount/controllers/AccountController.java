b58faa0be61e98120d0635df81101357984e7de4
Luis Kupferberg <lkupferberg@overactive.com>
2019-10-30 17:54:40 -0400

    -- Adding Mac files ignoring. -- Changing packages. -- Adding controller to start application. -- Adding database properties

---
+++ src/main/java/com/g2/personalaccount/controllers/AccountController.java
@@ -0,0 +1,8 @@
+package com.g2.personalaccount.controllers;
+
+/**
+ * @author Luis M. Kupferberg Ruiz (lkupferberg@overactive.com)
+ * @created 2019-10-30 17:41
+ */
+public class AccountController {
+}
